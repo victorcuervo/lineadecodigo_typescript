8f0eb4d1ab2f64497c6ed49d4f45d88a18579996
Víctor Cuervo <vcuervo@gmail.com>
2017-08-19 20:24:30 +0200

    Hola Mundo en TypeScript

---
+++ basicos/hola-mundo.ts
@@ -0,0 +1,11 @@
+/**
+ * @file hola-mundo.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   19/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/hola-mundo-typescript/
+ * @description Hola Mundo en el lenguaje TypeScript
+ */
+
+var mensaje:string = "Hola Mundo";
+console.log(mensaje);
