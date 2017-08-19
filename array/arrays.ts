391c78662d96a483bb58660e9199addb62ca8543
Víctor Cuervo <vcuervo@gmail.com>
2017-08-19 22:47:16 +0200

    Arrays en TypeScript

---
+++ array/arrays.ts
@@ -0,0 +1,17 @@
+/**
+ * @file arrays.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   19/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/arrays-en-typescript/
+ * @description Manejo de Arrays en TypeScript
+ */
+
+let miarray: number[] = [1,2,3,4,5];
+let otroarray: Array<number> = [1,2,3,4,5];
+
+// Mostrar un elemento
+console.log(miarray[1]);
+
+// Modificar un elemento
+miarray[1] = 8;
