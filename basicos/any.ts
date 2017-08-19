29b2ad831d843785ac13469d060a60e6d3278476
Víctor Cuervo <vcuervo@gmail.com>
2017-08-20 01:50:25 +0200

    Variables Any en TypeScript

---
+++ basicos/any.ts
@@ -0,0 +1,17 @@
+/**
+ * @file any.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   20/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/variables-any-en-typescript/
+ * @description Uso de variables Any en TypeScript
+ */
+
+let x:any = 3;
+console.log(x);
+
+x = 'soy una cadena de texto';
+console.log(x);
+
+x = true;
+console.log(x);
