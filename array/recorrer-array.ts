6f016b073805f7949533a9e80402db7ada8d922a
Víctor Cuervo <vcuervo@gmail.com>
2017-08-20 00:11:57 +0200

    Recorrer un Array en TypeScript mediante for...of

---
+++ array/recorrer-array.ts
@@ -0,0 +1,15 @@
+/**
+ * @file recorrer-array.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   19/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/recorrer-array-for-of-typescript/
+ * @description Recorrer Arrays en TypeScript mediante for of
+ */
+
+let miarray: number[] = [1,2,3,5,8];
+
+// Recorrer el aray
+for (let numero of miarray){
+  console.log(numero);
+}
