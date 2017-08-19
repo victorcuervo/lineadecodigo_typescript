02cd17d0323af50ddbcbaf449206c3c9f2a5c537
Víctor Cuervo <vcuervo@gmail.com>
2017-08-19 21:03:18 +0200

    Template String con TypeScript

---
+++ basicos/template-string.ts
@@ -0,0 +1,12 @@
+/**
+ * @file template-string.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   19/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/template-string-typescript/
+ * @description Cómo crear una plantilla de texto con TypeScript
+ */
+
+let lado:number = 2;
+let mensaje:string = `Un cuadrado de lado ${lado} tiene como área ${lado*2}`;
+console.log(mensaje);
