b56b7e139b0b26d862410e6f65a89bf464fdbced
Víctor Cuervo <vcuervo@gmail.com>
2017-08-20 23:21:33 +0200

    Cadenas de múltiples líneas en TypeScript

---
+++ basicos/cadena-multilinea.ts
@@ -0,0 +1,15 @@
+/**
+ * @file cadena-multilinea.ts
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   20/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/cadenas-de-multiples-lineas-en-typescript/
+ * @description Crear cadenas cuyo contenido sea multi línea.
+ */
+
+
+let cadena:string = `esto es una cadena
+que contiene
+múltiples líneas`;
+
+console.log(cadena);
